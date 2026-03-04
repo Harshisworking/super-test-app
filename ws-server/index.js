1258c87f68a243ad9a2563a76eacc660af809d74
Harshisworking <workwithharsh39@gmail.com>
2026-03-04 15:31:52 +0530

    Update index.js

--- ws-server/index.js
+++ ws-server/index.js
@@ -2,6 +2,7 @@ const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', (ws) => {
+    console.log(ws);
     ws.send(JSON.stringify({ event: 'connected', msg: 'Hello from WS Backend!' }));
     ws.on('message', (data) => console.log('received: %s', data));
 });
